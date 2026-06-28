ec30bbbdeb2a965ec564f174150a201cd08fe7a0
ProfMthLuiz <mthluiz99@gmail.com>
2026-06-28 16:09:50 -0300

    chore: initial setup with vite react typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Laravel from "./svg/laravel";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="skills">
+        <Laravel />
+        <p>Laravel</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
